0989a658d59264e6cadf46e2e243864bfb2d7778
imnotming <mzou.hawaii@gmail.com>
2025-03-30 18:40:36 -1000

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,15 +2,22 @@
 // import api to fetch dynamic links
 
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_DOMAIN_URI || 'https://pomomato.com',
+    siteUrl: 'https://pomomato.com/',
     generateRobotsTxt: true,
-    sitemapSize: 500,
     changefreq: 'daily',
     priority: 0.8,
     exclude: [
         '/auth/*',
         '/payment/*'
     ],
-    generateRobotsTxt: true, // Generate robots.txt
-    sitemapSize: 7000, // Split large sitemaps into multiple files
+    sitemapSize: 200, // Split large sitemaps into multiple files
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/auth/*', '/payment/*']
+            },
+        ],
+    },
 }
